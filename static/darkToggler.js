36f20f0a4b88d9344a8c234c932fcd7c86836337
Yohanes Bandung <bandungpenting@gmail.com>
2020-04-28 23:46:05 +0700

    style & refactor: dark 🌛 or light 🌞

--- static/darkToggler.js
+++ static/darkToggler.js
@@ -1,4 +1,21 @@
 (function() {
-  let toggler = document.getElementsByClassName("dark-toggler");
-  toggler.innerHTML = "🌛";
+  window.onload = function() {
+    console.log("Nice! You have the privilege of toggling the dark mode :D");
+    console.log("Just press the 🌞 or 🌛 logo on the top of page!");
+    const toggler = document.getElementById("dark-toggler");
+    const mainWrapper = document.getElementById("main-theme");
+
+    if (toggler && mainWrapper) {
+      toggler.className = "header__menu__list__toggler";
+      toggler.onclick = function() {
+        if (mainWrapper.className === "theme-light") {
+          mainWrapper.className = "theme-dark";
+          toggler.innerText = "🌛";
+        } else if (mainWrapper.className === "theme-dark") {
+          mainWrapper.className = "theme-light";
+          toggler.innerText = "🌞";
+        }
+      };
+    }
+  }
 })()
