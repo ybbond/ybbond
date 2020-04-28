6e2b18aebc810e61bcbd69bb1358faa8397cf714
Yohanes Bandung <bandungpenting@gmail.com>
2020-04-29 00:24:46 +0700

    hotfix: theme toggler absolute path

--- static/darkToggler.js
+++ static/darkToggler.js
@@ -1,21 +1,30 @@
 (function() {
   window.onload = function() {
-    console.log("Nice! You have the privilege of toggling the dark mode :D");
-    console.log("Just press the 🌞 or 🌛 logo on the top of page!");
     const toggler = document.getElementById("dark-toggler");
     const mainWrapper = document.getElementById("main-theme");
+    const currentTheme = localStorage.getItem("theme");
 
     if (toggler && mainWrapper) {
       toggler.className = "header__menu__list__toggler";
       toggler.onclick = function() {
         if (mainWrapper.className === "theme-light") {
           mainWrapper.className = "theme-dark";
+          localStorage.setItem("theme", "dark");
           toggler.innerText = "🌛";
         } else if (mainWrapper.className === "theme-dark") {
           mainWrapper.className = "theme-light";
+          localStorage.setItem("theme", "light");
           toggler.innerText = "🌞";
         }
       };
     }
+
+    if (currentTheme && currentTheme === "light") {
+      mainWrapper.className = "theme-light";
+      toggler.innerText = "🌞";
+    }
+
+    console.log("Nice! You have the privilege of toggling the dark mode :D");
+    console.log("Just press the 🌞 or 🌛 logo on the top of page!");
   }
 })()
