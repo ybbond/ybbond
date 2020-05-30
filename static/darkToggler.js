5825e1a05dbcb5ba34e3f0949a851dfdb50b9cea
Yohanes Bandung <hi@ybbond.dev>
2020-05-30 21:19:32 +0700

    refactor: upgrade to gatsby 71, and improve darkToggler load

--- static/darkToggler.js
+++ static/darkToggler.js
@@ -1,30 +1,28 @@
 (function() {
-  window.onload = function() {
-    const toggler = document.getElementById("dark-toggler");
-    const mainWrapper = document.getElementById("main-theme");
-    const currentTheme = localStorage.getItem("theme");
+  const toggler = document.getElementById("dark-toggler");
+  const mainWrapper = document.getElementById("main-theme");
+  const currentTheme = localStorage.getItem("theme");
 
-    if (toggler && mainWrapper) {
-      toggler.className = "header__menu__list__toggler";
-      toggler.onclick = function() {
-        if (mainWrapper.className === "theme-light") {
-          mainWrapper.className = "theme-dark";
-          localStorage.setItem("theme", "dark");
-          toggler.innerText = "🌛";
-        } else if (mainWrapper.className === "theme-dark") {
-          mainWrapper.className = "theme-light";
-          localStorage.setItem("theme", "light");
-          toggler.innerText = "🌞";
-        }
-      };
-    }
-
-    if (currentTheme && currentTheme === "light") {
-      mainWrapper.className = "theme-light";
-      toggler.innerText = "🌞";
-    }
-
-    console.log("Nice! You have the privilege of toggling the dark mode :D");
-    console.log("Just press the 🌞 or 🌛 logo on the top of page!");
+  if (toggler && mainWrapper) {
+    toggler.className = "header__menu__list__toggler";
+    toggler.onclick = function() {
+      if (mainWrapper.className === "theme-light") {
+        mainWrapper.className = "theme-dark";
+        localStorage.setItem("theme", "dark");
+        toggler.innerText = "🌛";
+      } else if (mainWrapper.className === "theme-dark") {
+        mainWrapper.className = "theme-light";
+        localStorage.setItem("theme", "light");
+        toggler.innerText = "🌞";
+      }
+    };
   }
+
+  if (currentTheme && currentTheme === "light") {
+    mainWrapper.className = "theme-light";
+    toggler.innerText = "🌞";
+  }
+
+  console.log("Nice! You have the privilege of toggling the dark mode :D");
+  console.log("Just press the 🌞 or 🌛 logo on the top of page!");
 })()
